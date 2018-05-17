aff937c5d208c5a940fec7860d2b1f00303ae676
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-05-17 23:36:45 +0200

    mutation createTalk, rendering TalkList

--- src/components/TalkDetail.js
+++ src/components/TalkDetail.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class TalkDetail extends Component{
     render(){
-        // TODO: Get _id and Rendering the details of Talks from API
+        // TODO: Get _id and Render the details of Talks from API
         return(
             <div>
                 Talk details
