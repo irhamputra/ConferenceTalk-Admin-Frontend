a8d50a59a245b0275929ff18df907e3424f8bc98
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-07-18 10:57:45 +0200

    ref name

--- src/components/AddTalk.js
+++ src/components/AddTalk.js
@@ -42,25 +42,25 @@ class AddTalk extends Component {
                                     placeholder="Title"/>
                                 <input
                                     type="text"
-                                    ref="name"
+                                    ref="description"
                                     onChange={e => this.setState({ description: e.target.value })}
                                     value={this.state.description}
                                     placeholder="Description"/>
                                 <input
                                     type="text"
-                                    ref="name"
+                                    ref="speakerName"
                                     onChange={e => this.setState({ speakerName: e.target.value })}
                                     value={this.state.speakerName}
                                     placeholder="Name of the speaker"/>
                                 <input
                                     type="text"
-                                    ref="name"
+                                    ref="conferenceName"
                                     onChange={e => this.setState({ conferenceName: e.target.value })}
                                     value={this.state.conferenceName}
                                     placeholder="Conference Name"/>
                                 <input
                                     type="text"
-                                    ref="name"
+                                    ref="video"
                                     onChange={e => this.setState({ video: e.target.value })}
                                     value={this.state.video}
                                     placeholder="Video URL"/>
